29dd0501f042cf6725b0213977c1dd54eaad4d3d
NickDeVisch <nick.de.visch@hotmail.com>
2023-05-16 20:23:09 +0200

    Test commit

--- Code/main.py
+++ Code/main.py
@@ -54,4 +54,6 @@ for i in range(int(video.get(cv2.CAP_PROP_FRAME_COUNT))):
 
     cv2.imshow('Video', ResizeImage(frame))
     cv2.waitKey(1)
-cv2.destroyAllWindows()        
+cv2.destroyAllWindows()   
+
+print(url)
